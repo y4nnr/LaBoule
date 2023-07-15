2cc0c9887296164e13e3eb53896b3f152e1e4689
y4nnr <yann.richard@mail.com>
2023-07-15 12:07:09 +0000

    Initial commit

---
+++ src/UnderConstruction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UnderConstruction = () => {
+  return (
+    <div>
+      <h1>Website Under Construction</h1>
+      <p>We are currently building this website. Please check back soon!</p>
+    </div>
+  );
+};
+
+export default UnderConstruction;
